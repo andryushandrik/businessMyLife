9a671bf5fd7d1fda58b8d259468e0a6596bed5b5
Andrey <72279405+andryushandrik@users.noreply.github.com>
2023-01-24 18:56:55 +0300

    Check Auth Header disabled

--- app/Middleware/CheckAuthHeaders.ts
+++ app/Middleware/CheckAuthHeaders.ts
@@ -1,23 +1,23 @@
 // * Types
-import type { Err } from 'Contracts/response'
+// import type { Err } from 'Contracts/response'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 // * Types
 
-import ExceptionService from 'App/Services/ExceptionService'
-import { ResponseCodes, ResponseMessages } from 'Config/response'
+// import ExceptionService from 'App/Services/ExceptionService'
+// import { ResponseCodes, ResponseMessages } from 'Config/response'
 
 export default class CheckAuthHeaders {
-	public async handle({ request }: HttpContextContract, next: () => Promise<void>) {
-		try {
-			if (!request.header('User-Agent') || !request.header('User-Fingerprint') || !request.ip())
-				throw {
-					code: ResponseCodes.CLIENT_ERROR,
-					message: ResponseMessages.MISS_AUTH_HEADERS,
-				} as Err
+	public async handle({  }: HttpContextContract, next: () => Promise<void>) {
+		// try {
+		// 	if (!request.header('User-Agent') || !request.header('User-Fingerprint') || !request.ip())
+		// 		throw {
+		// 			code: ResponseCodes.CLIENT_ERROR,
+		// 			message: ResponseMessages.MISS_AUTH_HEADERS,
+		// 		} as Err
 
-			await next()
-		} catch (err: Err | any) {
-			throw new ExceptionService(err)
-		}
+		await next()
+		// } catch (err: Err | any) {
+		// 	throw new ExceptionService(err)
+		// }
 	}
 }
