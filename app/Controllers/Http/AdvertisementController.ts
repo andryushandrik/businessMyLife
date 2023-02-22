23097c9b492f240d11b7ea8c5933ff17eaffeb0b
despair <grobamist@gmail.com>
2023-02-22 15:52:50 +0300

    redo ads

--- app/Controllers/Http/AdvertisementController.ts
+++ app/Controllers/Http/AdvertisementController.ts
@@ -1,3 +1,4 @@
+import Subsection from 'App/Models/Offer/Subsection'
 import { DateTime } from 'luxon'
 import AdvertisementValidator from 'App/Validators/Ads/AdvertisementValidator'
 import AdvertisementService from 'App/Services/AdvertisementService'
@@ -22,10 +23,10 @@ export default class AdvertisementController {
 				page,
 				limit,
 				baseUrl,
-				relations: ['owner'],
 			})
 			return await view.render('pages/ads/index', { ads })
 		} catch (err: Err | any) {
+      console.log(err)
 			session.flash('error', err.message)
 			return response.redirect().back()
 		}
@@ -33,14 +34,14 @@ export default class AdvertisementController {
 
 	public async create({ view }: HttpContextContract) {
 		const users: User[] = await User.query()
-		return await view.render('pages/ads/create', { users })
+		const subsections: Subsection[] = await Subsection.query()
+		return await view.render('pages/ads/create', { users, subsections })
 	}
 
 	public async update({ request, response, session, params }: HttpContextContract) {
 		const id: Advertisement['id'] = params.id
 		const payload = await request.validate(AdvertisementValidator)
 		payload.placedAt = DateTime.now()
-
 		try {
 			await AdvertisementService.update(id, payload)
 			session.flash('success', ResponseMessages.SUCCESS)
@@ -55,17 +56,19 @@ export default class AdvertisementController {
 		const id: Advertisement['id'] = params.id
 		try {
 			const users = await User.query()
+			const subsections: Subsection[] = await Subsection.query()
+
 			const item: Advertisement = await AdvertisementService.get(id)
 
-			return view.render('pages/ads/edit', { item, users })
+			return view.render('pages/ads/edit', { item, users, subsections })
 		} catch (err: Err | any) {
 			session.flash('error', err.message)
 			return response.redirect().back()
 		}
 	}
 	public async store({ request, response, session }: HttpContextContract) {
+    const payload = await request.validate(AdvertisementValidator)
 		try {
-			const payload = await request.validate(AdvertisementValidator)
 			payload.placedAt = DateTime.now()
 			await AdvertisementService.create(payload)
 
@@ -76,4 +79,18 @@ export default class AdvertisementController {
 			return response.redirect().back()
 		}
 	}
+	public async destroy({ params, response, session }: HttpContextContract) {
+		const id: Advertisement['id'] = params.id
+
+		try {
+			await AdvertisementService.delete(id)
+
+			session.flash('success', ResponseMessages.SUCCESS)
+			return response.redirect().back()
+		} catch (err: Err | any) {
+			session.flash('error', err.message)
+			return response.redirect().back()
+		}
+	}
 }
+
--- app/Validators/Ads/AdvertisementValidator.ts
+++ app/Validators/Ads/AdvertisementValidator.ts
@@ -7,6 +7,7 @@ import IndexValidator from '../IndexValidator'
 import { schema } from '@ioc:Adonis/Core/Validator'
 import { getUserIdRules } from '../Rules/User/user'
 import { getAdsDescriptionRules, getAdsFileOptions, getAdvertisementPlacedUntilValidator } from '../Rules/ads'
+import { getSubsectionIdRules } from '../Rules/Offer/subsection'
 
 export default class AdvertisementValidator extends IndexValidator {
 	constructor(protected ctx: HttpContextContract) {
@@ -24,8 +25,9 @@ export default class AdvertisementValidator extends IndexValidator {
 		 * * Optional fields
 		 */
 
-		offerImage: schema.file.optional(getAdsFileOptions()),
-		subsectionImage: schema.file.optional(getAdsFileOptions()),
+		image: schema.file.optional(getAdsFileOptions()),
+    place: schema.enum(['offer','category']),
+    subsectionId: schema.number(getSubsectionIdRules())
 	})
 
 	public messages: CustomMessages = this.messages
